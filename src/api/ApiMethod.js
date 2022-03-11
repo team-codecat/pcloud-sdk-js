5809c9109b2b760cfa0f7d5839c69cd2148e5823
Leon Ryan <leon@codecat.com.au>
2022-03-12 09:32:15 +1100

    remove locationid from apimethod

--- src/api/ApiMethod.js
+++ src/api/ApiMethod.js
@@ -23,7 +23,7 @@ export default function ApiMethod(method: string, options: ApiMethodOptions = {}
 
   const requestUrl: string = url.format({
     protocol: apiProtocol,
-    host: locationid ? locations[locationid] : apiServer,
+    host: apiServer,
     pathname: method,
     query: params,
   });
