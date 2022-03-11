2d78784c600fd4e1df9c068c2cd92ca462e42ea2
Leon Ryan <leon@codecat.com.au>
2022-03-11 17:47:01 +1100

    add check for location id to ApiMethod

--- src/api/ApiMethod.js
+++ src/api/ApiMethod.js
@@ -23,7 +23,7 @@ export default function ApiMethod(method: string, options: ApiMethodOptions = {}
 
   const requestUrl: string = url.format({
     protocol: apiProtocol,
-    host: locations[locationid] || apiServer,
+    host: locationid ? locations[locationid] : apiServer,
     pathname: method,
     query: params,
   });
